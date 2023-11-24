0690ec985692df5ab2b79301ef18e92fc3a0feda
pdelboca <patriciodelboca@gmail.com>
2023-11-24 15:47:30 +0100

    Generate ckan.ini file

--- startup.sh
+++ startup.sh
@@ -4,14 +4,14 @@
 apt-get install libmagic-dev -y
 
 # Set up CKAN Config File
-# export CKAN_INI=ckan.ini
-# ckan generate config ${CKAN_INI}
+export CKAN_INI=ckan.ini
+ckan generate config ${CKAN_INI}
 
-# ckan config-tool ${CKAN_INI} "SECRET_KEY=${CKAN_SECRET_KEY}"
-# ckan config-tool ${CKAN_INI} "beaker.session.secret=${CKAN_BEAKER_SESSION_SECRET}"
-# ckan config-tool ${CKAN_INI} "sqlalchemy.url = ${CKAN_SQLALCHEMY_URL}"
-# ckan config-tool ${CKAN_INI} "ckan.redis.url = ${CKAN_CKAN_REDIS_URL}"
-# ckan config-tool ${CKAN_INI} "solr_url = ${CKAN_SOLR_URL}"
-# ckan config-tool ${CKAN_INI} "ckan.site_url = ${CKAN_SITE_URL}"
+ckan config-tool ${CKAN_INI} "SECRET_KEY=${CKAN_SECRET_KEY}"
+ckan config-tool ${CKAN_INI} "beaker.session.secret=${CKAN_BEAKER_SESSION_SECRET}"
+ckan config-tool ${CKAN_INI} "sqlalchemy.url = ${CKAN_SQLALCHEMY_URL}"
+ckan config-tool ${CKAN_INI} "ckan.redis.url = ${CKAN_CKAN_REDIS_URL}"
+ckan config-tool ${CKAN_INI} "solr_url = ${CKAN_SOLR_URL}"
+ckan config-tool ${CKAN_INI} "ckan.site_url = ${CKAN_SITE_URL}"
 
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
