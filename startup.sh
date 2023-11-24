c103266e3f3d4bf168c38f87e7468954bb869274
pdelboca <patriciodelboca@gmail.com>
2023-11-24 15:39:43 +0100

    Install libmagic-dev

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 # This file is run by Azure App Service as entrypoint
 # https://learn.microsoft.com/en-us/azure/app-service/configure-language-python#customize-startup-command
 
-# # Set up CKAN Config File
+apt-get install libmagic-dev
+
+# Set up CKAN Config File
 # export CKAN_INI=ckan.ini
 # ckan generate config ${CKAN_INI}
 
@@ -12,4 +14,4 @@
 # ckan config-tool ${CKAN_INI} "solr_url = ${CKAN_SOLR_URL}"
 # ckan config-tool ${CKAN_INI} "ckan.site_url = ${CKAN_SITE_URL}"
 
-# gunicorn --bind=0.0.0.0 --timeout 600 app:app
+gunicorn --bind=0.0.0.0 --timeout 600 app:app
